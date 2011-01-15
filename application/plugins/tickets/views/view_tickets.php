87fcd5dc73f146998c66d6c357894e4d157b1830
Dan Bernardic <dan@bernardic.ca>
2011-01-15 07:48:46 -0600

    Modified the HTML and CSS to give the tickets table a shiny header like the other tables have. Issue reported at http://www.projectpier.org/node/2002

--- application/plugins/tickets/views/view_tickets.php
+++ application/plugins/tickets/views/view_tickets.php
@@ -2,13 +2,13 @@
 <?php if (isset($ticketsheader)) { echo "<div class=\"tickets_header\">$ticketsheader</div>"; } ?>
 <table width="100%" cellpadding="2" border="0">
   <tr bgcolor>
-    <td width="40"><?php echo lang("ticket") ?></td>
-    <td><?php echo lang("summary") ?></td>
-    <td width="95"><?php echo lang("type") ?></td>
-    <td width="60"><?php echo lang("state") ?></td>
-    <td width="115"><?php echo lang("category") ?></td>
-    <td width="60" align="center"><?php echo ucfirst(lang("created by")) ?></td>
-    <td width="60" align="center"><?php echo lang("assigned to") ?></td>
+    <th width="40"><?php echo lang("ticket") ?></th>
+    <th><?php echo lang("summary") ?></th>
+    <th width="75"><?php echo lang("type") ?></th>
+    <th width="60"><?php echo lang("state") ?></th>
+    <th width="115"><?php echo lang("category") ?></th>
+    <th width="70" align="center"><?php echo ucfirst(lang("created by")) ?></th>
+    <th width="70" align="center"><?php echo lang("assigned to") ?></th>
   </tr>
 <?php foreach($tickets as $ticket) { ?>
   <tr class="<?php echo $ticket->getPriority(); ?>">
